6b233033e4d96db5a9289595b81cb83299cd84c6
Brant <viniassis_brantcouto@hotmail.com>
2022-04-25 22:20:41 -0300

    1° commit teste e adição e inclusão do Typescript

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,11 +1,11 @@
-import logo from './assets/logo.png';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        Olá mundo!!
       </header>
     </div>
   );
